828d16e6493c0ecd55065784998223bd3a03a090
Linkruste <74532664+Linkruste@users.noreply.github.com>
2023-06-02 23:34:39 +0200

    Initial commit

---
+++ Key.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcHacker
+{
+    internal class Key
+    {
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+        public string Path { get ; private set; }
+
+        public Key(string name, string value, string path)
+        {
+            Name = name;
+            Value = value;
+            Path = path;
+        }
+
+        public string ToCommandString() => $"-Path \"{Path}\" -Name \"{Name}\" -Value \"{Value}\"";
+        public string ToGetString() => $"-Path \"{Path}\" -Name \"{Name}\"";
+    }
+}
---
+++ RegistryManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ProcHacker
+{
+    internal static class RegistryManager
+    {
+        public static bool OverWriteKey(Key _infos)
+        {
+            string _command = $"Set-ItemProperty {_infos.ToCommandString()}";
+            Process _powerShell = PowerShellQuickStart(_command);
+            _powerShell.Start();
+            string _errors = _powerShell.StandardError.ReadToEnd();
+            _powerShell.WaitForExit();
+            if (!string.IsNullOrEmpty(_errors))
+            {
+                OutputRegistryError(_errors);
+                return false;
+            }
+            return true;
+        }
+
+        public static string ReadKey(Key _readable)
+        {
+            string _command = $"Set-ItemProperty {_readable.ToGetString()}";
+            Process _powerShell = PowerShellQuickStart(_command);
+            _powerShell.Start();
+            string _output = _powerShell.StandardOutput.ReadToEnd();
+            string _errors = _powerShell.StandardError.ReadToEnd();
+            _powerShell.WaitForExit();
+            if (!( string.IsNullOrEmpty(_errors) && string.IsNullOrEmpty(_output)))
+                OutputRegistryError($"Couldn't get any output or following error(s) was found:\n{_errors}");
+            return _output;
+        }
+
+        static Process PowerShellQuickStart(string _command) => 
+            new Process()
+            {
+                StartInfo =
+                    new ProcessStartInfo()
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $"{_command}",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+            };
+
+        private static void OutputRegistryError(string _error) => MessageBox.Show(_error, "Registry Error");
+    }
+}
